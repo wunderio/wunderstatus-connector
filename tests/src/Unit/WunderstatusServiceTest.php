133e0f686325562f5bad0ec21b5e8044dcd2704b
Jani Palsamäki <jani.palsamaki@gmail.com>
2016-07-11 11:07:48 +0300

    Added more unit tests for WunderstatusService.

--- tests/src/Unit/WunderstatusServiceTest.php
+++ tests/src/Unit/WunderstatusServiceTest.php
@@ -2,28 +2,54 @@
 
 namespace Drupal\Tests\wunderstatus\Unit;
 
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Logger\LoggerChannel;
 use Drupal\Core\State\StateInterface;
 use Drupal\wunderstatus\WunderstatusService;
 use Drupal\Tests\UnitTestCase;
 use Drupal\wunderstatus\WunderstatusInfoCollector;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Prophecy\Argument;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
 
 /**
  * @group wunderstatus
  */
 class WunderstatusServiceTest extends UnitTestCase {
+
+  const MODULES = ['module_a 1.0', 'module_b 2.1'];
+  const SITE_NAME = 'Site name';
+  const SITE_UUID = 'Site UUID';
+  const WUNDERSTATUS_AUTH_PASSWORD = 'Password';
+  const WUNDERSTATUS_AUTH_USERNAME = 'Username';
+  const WUNDERSTATUS_KEY = 'Key';
+  const WUNDERSTATUS_MANAGER_ENDPOINT_URL = 'http://www.example.com';
   
   /** @var Client */
   private $client;
+
+  /** @var ImmutableConfig */
+  private $config;
+
+  /** @var ConfigFactory */
+  private $configFactory;
 
   /** @var ContainerInterface */
   private $container;
 
   /** @var LoggerChannel */
   private $logger;
+
+  /** @var RequestInterface */
+  private $request;
+
+  /** @var ResponseInterface */
+  private $response;
 
   /** @var StateInterface */
   private $state;
@@ -37,15 +63,32 @@ class WunderstatusServiceTest extends UnitTestCase {
   public function setUp() {
     parent::setUp();
 
+    $this->request = $this->prophesize(RequestInterface::class);
+    $this->response = $this->prophesize(ResponseInterface::class);
+
     $this->client = $this->prophesize(Client::class);
+    $this->client->request('POST', self::WUNDERSTATUS_MANAGER_ENDPOINT_URL, Argument::any())->willReturn($this->response);
+
+    $this->config = $this->prophesize(ImmutableConfig::class);
+    $this->config->get('name')->willReturn(self::SITE_NAME);
+    $this->config->get('uuid')->willReturn(self::SITE_UUID);
+
+    $this->configFactory = $this->prophesize(ConfigFactory::class);
+    $this->configFactory->get('system.site')->willReturn($this->config);
+
     $this->logger = $this->prophesize(LoggerChannel::class);
+
     $this->wunderstatusInfoCollector = $this->prophesize(WunderstatusInfoCollector::class);
+    $this->wunderstatusInfoCollector->getVersionInfo()->willReturn(self::MODULES);
 
     $this->state = $this->prophesize(StateInterface::class);
-    $this->state->get('wunderstatus_key')->willReturn('Key');
-    $this->state->get('wunderstatus_manager_endpoint_url')->willReturn('http://www.example.com/');
+    $this->state->get('wunderstatus_auth_password')->willReturn(self::WUNDERSTATUS_AUTH_PASSWORD);
+    $this->state->get('wunderstatus_auth_username')->willReturn(self::WUNDERSTATUS_AUTH_USERNAME);
+    $this->state->get('wunderstatus_key')->willReturn(self::WUNDERSTATUS_KEY);
+    $this->state->get('wunderstatus_manager_endpoint_url')->willReturn(self::WUNDERSTATUS_MANAGER_ENDPOINT_URL);
 
     $this->container = $this->prophesize(ContainerInterface::class);
+    $this->container->get('config.factory')->willReturn($this->configFactory);
     $this->container->get('state')->willReturn($this->state);
     \Drupal::setContainer($this->container->reveal());
     
@@ -80,5 +123,57 @@ class WunderstatusServiceTest extends UnitTestCase {
     $response = $this->wunderstatusService->sendModuleInfo();
 
     $this->assertFalse($response);
+  }
+
+  /**
+   * @test
+   */
+  public function sendModuleInfoShouldGetVersionInfoFromWunderstatusInfoCollector() {
+    $this->wunderstatusInfoCollector->getVersionInfo()->shouldBeCalled();
+
+    $this->wunderstatusService->sendModuleInfo();
+  }
+
+  /**
+   * @test
+   */
+  public function sendModuleInfoShouldBuildRequestOptionsCorrectly() {
+    $expectedRequestOptions = [
+      'auth' => [
+        self::WUNDERSTATUS_AUTH_USERNAME,
+        self::WUNDERSTATUS_AUTH_PASSWORD
+      ],
+      'body' => Json::encode([
+        'key' => self::WUNDERSTATUS_KEY,
+        'modules' => self::MODULES,
+        'siteName' => self::SITE_NAME,
+        'siteUuid' => self::SITE_UUID
+      ])
+    ];
+
+    $this->client->request(Argument::any(), Argument::any(), $expectedRequestOptions)->shouldBeCalled();
+
+    $this->wunderstatusService->sendModuleInfo();
+  }
+
+  /**
+   * @test
+   */
+  public function sendModuleInfoShouldReturnOkResponseOnSuccess() {
+    $response = $this->wunderstatusService->sendModuleInfo();
+
+    $this->assertInstanceOf(ResponseInterface::class, $response);
+  }
+
+  /**
+   * @test
+   */
+  public function sendModuleShouldHandleExceptionsGracefully() {
+    $message = 'Exception';
+    $this->client->request(Argument::any(), Argument::any(), Argument::any())->willThrow(new RequestException($message, $this->request->reveal()));
+
+    $response = $this->wunderstatusService->sendModuleInfo();
+
+    $this->assertInstanceOf(ResponseInterface::class, $response);
   }
 }
